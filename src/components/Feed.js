c5e13f8c34240291f701f0c001e2ef7d03dfd61a
Munawarhussain729 <muanwarhussa.7@gmail.com>
2023-01-22 23:00:13 +0500

    done with Side Bar too

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import { Stack } from '@mui/material'
+import React from "react";
+import { Stack, Box, Typography } from "@mui/material";
+import SideBar from "./SideBar";
 const Feed = () => {
   return (
-  <Stack>
+    <Stack
+      sx={{
+        flexDirection: {
+          sx: "column",
+          md: "row",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar/>
+        <Typography className="copyright" variant="body2" sx={{mt:1.5, color:"white"}}>
+          Copyright 2023 RYT
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-  </Stack>
-  )
-}
-
-export default Feed
+export default Feed;
